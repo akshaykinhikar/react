2cedd78c2d476d2634e2df507cc71603cccd2dd9
akshaykinhikar <kinhikar.akshay32@gmail.com>
2020-08-12 23:34:04 +0530

    state and binding simple example

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -3,14 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: 'Hello, this is default data from state'
+    }
+  }
   render() {
     return (
           <div className="App">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Used component
+                {this.state.data}
               </p>
+              <button onClick={()=>{this.setState({'data': 'this is new message on button Click'})}}>Click to change text</button>
               <a
                 className="App-link"
                 href="https://reactjs.org"
