f8f73aa847406e6d2a88e09fb59c90ef91b2557c
akshaykinhikar <kinhikar.akshay32@gmail.com>
2020-08-13 00:43:53 +0530

    map function use

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -7,26 +7,27 @@ class App extends Component {
     super();
 
     this.state = {
-      data: 'Hello, this is default data from state'
+      data: [
+        {name: 'Lorem', id:'1',  profileURL: 'https://i0.wp.com/www.repol.copl.ulaval.ca/wp-content/uploads/2019/01/default-user-icon.jpg'},
+        {name: 'Ipsome', id:'2',  profileURL: 'https://i0.wp.com/www.repol.copl.ulaval.ca/wp-content/uploads/2019/01/default-user-icon.jpg'},
+        {name: 'Tony', id:'3',  profileURL: 'https://i0.wp.com/www.repol.copl.ulaval.ca/wp-content/uploads/2019/01/default-user-icon.jpg'}
+      ]
     }
   }
   render() {
     return (
           <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                {this.state.data}
-              </p>
-              <button onClick={()=>{this.setState({'data': 'this is new message on button Click'})}}>Click to change text</button>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <div>
+                {
+                  this.state.data.map(user => (
+                    <div key={user.id}>
+                      <img key={user.id + 'img'} src={user.profileURL} className="user-image"/>
+                      <h1 key={user.id + 'name'}>{user.name}</h1>
+                    </div>
+                  ))
+                }
+              </div>
             </header>
           </div>
         );
